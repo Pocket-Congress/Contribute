879794d8eedaa0025fd6887fc75e4479b8be56b7
Gavin <grmontes91021@gmail.com>
2024-06-12 18:48:14 -0700

    CongressDotGov API proof of concept

--- CongressDotGov/Package.swift
+++ CongressDotGov/Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "SampleContribution",
+    name: "CongressDotGov",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SampleContribution",
-            targets: ["SampleContribution"]),
+            name: "CongressDotGov",
+            targets: ["CongressDotGov"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SampleContribution"),
+            name: "CongressDotGov"),
         .testTarget(
-            name: "SampleContributionTests",
-            dependencies: ["SampleContribution"]),
+            name: "CongressDotGovTests",
+            dependencies: ["CongressDotGov"]),
     ]
 )
